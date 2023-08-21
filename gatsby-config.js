4002f015195fe0238470a266671e6c51fc9dee16
16010948 <bjyoung0@gmail.com>
2023-08-21 22:54:01 +0900

    [config] Typescript 설정

---
+++ gatsby-config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Blog`,
+    description: `Gatsby Blog`,
+    author: `100g0`,
+  },
+  plugins: [
+    {
+      resolve: "gatsby-plugin-typescript",
+      options: {
+        isTSX: true,
+        allExtensions: true,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `contents`,
+        path: `${__dirname}/contents`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+  ],
+}
--- src/pages/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const IndexPage = () => (
-    <>
-        <h1>GATSBY STARTER PAGE</h1>
-    </>
-);
-
-export default IndexPage;
