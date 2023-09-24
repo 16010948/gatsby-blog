6392abc4c16dfc60c89f690a0b9b60f6ac8e5b71
16010948 <bjyoung0@gmail.com>
2023-09-24 23:43:00 +0900

    [fix] 빌드 오류 수정

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,7 @@
+import React, { FunctionComponent } from 'react'
 import styled from '@emotion/styled'
 import { Link } from 'gatsby'
+import GlobalStyle from '@components/Common/GlobalStyle'
 
 const NotFoundPageWrapper = styled.div`
   display: flex;
@@ -37,9 +39,19 @@ const GoToMainButton = styled(Link)`
     text-decoration: underline;
   }
 `
-export {
-  NotFoundPageWrapper,
-  NotFoundText,
-  NotFoundDescription,
-  GoToMainButton,
+
+const NotFoundPage: FunctionComponent = function () {
+  return (
+    <NotFoundPageWrapper>
+      <GlobalStyle />
+      <NotFoundText>404</NotFoundText>
+      <NotFoundDescription>
+        찾을 수 없는 페이지입니다. <br />
+        다른 콘텐츠를 보러 가보시겠어요?
+      </NotFoundDescription>
+      <GoToMainButton to="/">메인으로</GoToMainButton>
+    </NotFoundPageWrapper>
+  )
 }
+
+export default NotFoundPage
